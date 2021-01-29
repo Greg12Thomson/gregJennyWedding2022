dc64d0fe23fca6fab05cd44ff880b8121e0e792a
Greg Thomson <Greg12Thomson@Gmail.com>
2021-01-29 22:01:17 +0000

    Add formal invitation entry point

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
 import React from "react";
 
 import './App.css';
-// import NavBar from './components/NavBar';
-import {VipHeader, VipContent} from './components/Vip';
-// import Footer from './components/Footer';
+import NavBar from './components/NavBar';
+// import {VipHeader, VipContent} from './components/Vip';
+import Footer from './components/Footer';
 
 function App() {
   return (
     // once VIP is done!
-    // <div className="App">
-    //   <NavBar />
-    //   <Footer />
-    // </div>
-
     <div className="App">
-      <>
-        <VipHeader />
-        <VipContent />
-      </>
+      <NavBar />
+      <Footer />
     </div>
+
+    // <div className="App">
+    //   <>
+    //     <VipHeader />
+    //     <VipContent />
+    //   </>
+    // </div>
   );
 }
 
