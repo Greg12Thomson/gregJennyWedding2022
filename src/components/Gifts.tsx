3a913b54247717ad4f344307470200a548cde663
GregisGinger <Greg12thomson@Gmail.com>
2022-08-11 20:43:51 +0100

    Style fix

--- src/components/Gifts.tsx
+++ src/components/Gifts.tsx
@@ -10,10 +10,10 @@ function Gifts() {
                     Gifts
                 </h1>
                 <Row>
-                    <Col className="her-side" xs={12} md={6}>
+                    <Col className="her-side" xs={12} md={4}>
                         <Image src={VanImage} thumbnail/>
                     </Col>
-                    <Col className="gifts" xs={12} md={6} >
+                    <Col className="gifts" xs={12} md={8} >
                         <p>
                             We have been together for a long time so don’t need anything for our home, we really just  appreciate you making the journey and time to celebrate our wedding day with us. <br/>
                             Therefore we do not want to ask for any gifts, but if you really want to, a little bit of money towards our 2023 adventure is all we really need.
