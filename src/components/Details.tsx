4c5046a5d400394fe33c50dbcb3aee1963be7faa
GregisGinger <Greg12thomson@Gmail.com>
2022-08-11 20:25:02 +0100

    Update runnign order

--- src/components/Details.tsx
+++ src/components/Details.tsx
@@ -1,10 +1,6 @@
 import React from 'react';
 import {Col, Container, Row, Image} from "react-bootstrap";
-import Knot from "../images/Knot.png";
-import Glasses from "../images/Glasses.png";
-import Cambo from "../images/Cambo.png";
-import Cutlery from "../images/Cutlery.png";
-import Disco from "../images/Disco.png";
+import RunningOrder from "../images/Running-order-1.png";
 
 function Details() {
     return (
@@ -14,43 +10,12 @@ function Details() {
                     Wedding Details
                 </h1>
                 <Row>
-                    <Col className="when" xs={6} md={6}>
-                        <Row>
-                            <Image src={Cambo} />
-                            <p>
-                                Arrival - 12:30
-                            </p>
-                        </Row>
-                        <Row>
-                            <Image src={Glasses} />
-                            <p>
-                                Drinks - 14:00
-                            </p>
-                        </Row>
-                        <Row>
-                            <Image src={Disco} />
-                            <p>
-                                Party - 19:30
-                            </p>
-                        </Row>
+                    <Col className="running-order" md={2}>
                     </Col>
-                    <Col className="where" xs={6} md={6} >
-                        <Row>
-                            <p>
-                            </p>
-                        </Row>
-                        <Row>
-                            <p className="first">
-                                13:00 - Ceremony
-                            </p>
-                            <Image src={Knot} />
-                        </Row>
-                        <Row>
-                            <p>
-                                16:30 - Dinner
-                            </p>
-                            <Image src={Cutlery} />
-                        </Row>
+                    <Col className="running-order" md={8}>
+                        <Image src={RunningOrder} />
+                    </Col>
+                    <Col className="running-order" md={2}>
                     </Col>
                 </Row>
             </Container>
